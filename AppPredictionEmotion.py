cd1077a8c72563d3fff8a5b26eca45c52482eb5a
Jaroslav Kotrba <hp@Jaroslavs-MacBook-Air.local>
2022-07-10 12:11:10 +0200

    changes

--- AppPredictionEmotion.py
+++ AppPredictionEmotion.py
@@ -4,17 +4,24 @@
 # cd "/Users/hp/OneDrive/Documents/Python Anaconda/Streamlit_NLP_App/AppPredictionEmotion"
 # streamlit run AppPredictionEmotion.py
 
+# Track Utils
+from track_utils import create_page_visited_table, add_page_visited_details, view_all_page_visited_details, add_prediction_details, view_all_prediction_details, create_emotionclf_table
+
+# Streamlit
 import streamlit as st
 import altair as alt
+import plotly.express as px
 
+# Basic
 import numpy as np
 import pandas as pd
 from sklearn import pipeline
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 100)
+from datetime import datetime
 
+# Load model
 import joblib
-
 pipe = joblib.load(open('models/saved_pipeline.pkl', 'rb'))
 
 def predict_emotion(x):
@@ -27,66 +34,88 @@ def predict_emotions_proba(x):
 
 emotions_emoji_dict = {"anger":"😠","disgust":"🤮", "fear":"😨😱", "happy":"🤗", "joy":"😂", "neutral":"😐", "sad":"😔", "sadness":"😔", "shame":"😳", "surprise":"😮"}
 
-data = pd.read_csv("data/emotion_dataset.csv")
-
 def main():
     # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
     # animations: https://lottiefiles.com/search?q=pc&category=animations
     st.set_page_config(page_icon=":book:", layout="wide")
 
-    # st.sidebar.title("NAVIGATION")
+    st.sidebar.title("NAVIGATION")
 
-    # menu = ["Home", "Monitor", "About"]
+    menu = ["Home", "Monitor", "About"]
     
-    # choice = st.sidebar.radio("Menu", menu)
+    choice = st.sidebar.selectbox("Menu", menu)
 
-    # if choice == "Home":
-    st.subheader("Emotion in Text")
-    with st.form(key='emotion_clf_form', clear_on_submit=True):
-            raw_text = st.text_area("Type here:")
-            submit_text = st.form_submit_button(label="Submit")
+    if choice == "Home":
+        st.subheader("Emotion in Text")
+        with st.form(key='emotion_clf_form', clear_on_submit=True):
+                raw_text = st.text_area("Type here:")
+                submit_text = st.form_submit_button(label="Submit")
 
-    if (submit_text and raw_text==""):
-        st.warning("Please fill the form!")
-    else: 
-        if submit_text:
-            left_column, right_column = st.columns(2)
+        if (submit_text and raw_text==""):
+            st.warning("Please fill the form!")
+        else: 
+            if submit_text:
+                left_column, right_column = st.columns(2)
 
-            # Model
-            prediction = predict_emotion(raw_text)
-            probability = predict_emotions_proba(raw_text)
+                # Model
+                prediction = predict_emotion(raw_text)
+                probability = predict_emotions_proba(raw_text)
 
-            # Left side
-            with left_column:
-                st.success("Original Text")
-                st.write(f'<p style="color:#0091EA">{raw_text}</p>', unsafe_allow_html=True)
+                # Left side
+                with left_column:
+                    st.success("Original Text")
+                    st.write(f'<p style="color:#0091EA">{raw_text}</p>', unsafe_allow_html=True)
 
-                st.success("Prediction")
-                emoji_icon = emotions_emoji_dict[prediction]
-                st.write("{} {}".format(prediction, emoji_icon))
-                st.write("Confidence: {:.4f}".format(np.max(probability)))
+                    st.success("Prediction")
+                    emoji_icon = emotions_emoji_dict[prediction]
+                    st.write("{} {}".format(prediction, emoji_icon))
+                    st.write("Confidence: {:.4f}".format(np.max(probability)))
 
-            # Rigth side
-            with right_column:
-                st.success("Prediction Probability")
-                proba_df = pd.DataFrame(probability, columns=pipe.classes_)
-                # st.write(proba_df)
-                proba_df_clean = proba_df.T.reset_index()
-                proba_df_clean.columns = ["emotions", "probability"]
+                # Rigth side
+                with right_column:
+                    st.success("Prediction Probability")
+                    proba_df = pd.DataFrame(probability, columns=pipe.classes_)
+                    proba_df_clean = proba_df.T.reset_index()
+                    proba_df_clean.columns = ["emotions", "probability"]
 
-                fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
-                st.altair_chart(fig, use_container_width=True)
+                    fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
+                    st.altair_chart(fig, use_container_width=True)
 
-        # elif choice == "Monitor":
-        #     st.subheader("Monitor App")
+    elif choice == "Monitor":
+        st.subheader("Monitor App")
+        add_page_visited_details("Monitor",datetime.now())
 
-        # elif choice == "About":
-        #     st.subheader("About")
-        
-        # st.sidebar.markdown("""---""")
+        with st.beta_expander("Page Metrics"):
+            page_visited_details = pd.DataFrame(view_all_page_visited_details(),columns=['Pagename','Time_of_Visit'])
+            st.dataframe(page_visited_details)	
 
-        # st.sidebar.subheader("More info:"); 
-        # st.sidebar.write(":computer: https://jaroslavkotrba.com")
+            pg_count = page_visited_details['Pagename'].value_counts().rename_axis('Pagename').reset_index(name='Counts')
+            c = alt.Chart(pg_count).mark_bar().encode(x='Pagename',y='Counts',color='Pagename')
+            st.altair_chart(c,use_container_width=True)	
+
+            p = px.pie(pg_count,values='Counts',names='Pagename')
+            st.plotly_chart(p,use_container_width=True)
+
+
+
+
+
+
+
+    elif choice == "About":
+        st.subheader("About")
+
+
+
+
+
+
+
+            
+    st.sidebar.markdown("""---""")
+
+    st.sidebar.subheader("More info:"); 
+    st.sidebar.write("https://jaroslavkotrba.com")
 
     st.markdown("""---""")
 
